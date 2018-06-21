8cb01606e346b9654d752fada74dd168fe0ca8a6
crosssh <crosssh@oregonstate.edu>
2018-06-21 10:52:51 -0700

    able to register new users.

--- src/reducers/user.reducer.ts
+++ src/reducers/user.reducer.ts
@@ -2,8 +2,14 @@ import { IUser } from ".";
 import { userTypes } from "../actions/user/user.types";
 
 const initialState: IUser = {
+  attribute: {
+    email: "",
+    family_name: "",
+    given_name: "",
+  },
   authToken: "",
   errorMessage: "",
+  password: "",
   username: ""
 };
 
@@ -26,6 +32,35 @@ export const userReducer = (state = initialState, action: any) => {
         ...state,
         username: action.payload.username
       };
+      case userTypes.UPDATE_EMAIL:
+      return {
+        ...state,
+        attribute: {
+          ...state.attribute,
+          email: action.payload.email
+        }
+      };
+      case userTypes.UPDATE_FAMILY_NAME:
+      return {
+        ...state,
+        attribute: {
+          ...state.attribute,
+          family_name: action.payload.familyName
+        }
+      };
+      case userTypes.UPDATE_GIVEN_NAME:
+      return {
+        ...state,
+        attribute: {
+          ...state.attribute,
+          given_name: action.payload.givenName
+        }
+      };
+      case userTypes.UPDATE_PASSWORD:
+        return {
+          ...state,
+          password: action.payload.password
+        };
   }
 
   return state;
