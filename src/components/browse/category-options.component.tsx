c46a529259c16ca71df7260fa7fe66fc261806a3
luis.gonzalez.tirado87@gmail.com <luis.gonzalez.tirado87@gmail.com>
2018-06-22 15:43:32 -0400

    added seach by type ,name, category

---
+++ src/components/browse/category-options.component.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+export const CategotyOptions= ()=> (    
+
+<div>
+<select>
+<option value="volvo">Category1</option>
+<option value="saab">Categoty2</option>
+<option value="mercedes">Category3</option>
+</select>
+</div>
+
+)
+
+   
+     
