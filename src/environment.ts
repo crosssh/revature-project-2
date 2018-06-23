3e68267f788aa4aec36d318a08bd628ad5fcd173
RGWantz <ruthanna.wantz17@houghton.edu>
2018-06-22 20:21:18 -0400

    ran build again

--- src/environment.ts
+++ src/environment.ts
@@ -1,11 +1,9 @@
 const dev = {
-  context: 'https://8lomsjt0a6.execute-api.us-west-2.amazonaws.com/dev/'
-}
+  context: "https://8lomsjt0a6.execute-api.us-west-2.amazonaws.com/dev/"
+};
 
 const prod = {
-  context: 'http://ec2-34-219-112-62.us-west-2.compute.amazonaws.com:3001/'
-}
+  context: "https://8lomsjt0a6.execute-api.us-west-2.amazonaws.com/dev/"
+};
 
-export const environment = process.env.NODE_ENV === 'production'
-  ? prod
-  : dev
+export const environment = process.env.NODE_ENV === "production" ? prod : dev;
