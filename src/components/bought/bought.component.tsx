efc8e2fcf97fcfbf82a01a7a89057a517a7433ce
RGWantz <ruthanna.wantz17@houghton.edu>
2018-06-25 15:46:59 -0400

    bought structure in place but not working

--- src/components/bought/bought.component.tsx
+++ src/components/bought/bought.component.tsx
@@ -1,41 +1,98 @@
 import * as React from "react";
 import { ProfileNavComponent } from "../profile-nav.component";
-import { IBuyer, IUser } from "../../reducers";
+import { IBuyer, IUser, IProduct } from "../../reducers";
 
-interface IProp extends IBuyer, IUser {
+interface IProp extends IBuyer, IProduct, IUser {
   buyer: any;
+  product: any;
   user: any;
-  getBuyer: (username: string) => void;
+  getBuyer: (username: string) => Promise<any>;
+  getBySellerAndTime: (username: string, timePosted: number) => Promise<any>;
 }
 
 export class BoughtComponent extends React.Component<IProp, any> {
   constructor(props: any) {
     super(props);
-    console.log(props);
+    this.state = {
+      itemList: []
+    };
   }
 
   public componentDidMount() {
-    this.props.getBuyer(this.props.user.username);
+    this.props.getBuyer(this.props.user.username)
+      .then(resp => {
+        for (let i = 0; i < this.props.buyer.currentBuyer.boughtItems.length; i++) {
+          this.props
+            .getBySellerAndTime(
+              this.props.buyer.currentBuyer.boughtItems[i].seller,
+              this.props.buyer.currentBuyer.boughtItems[i].timePosted
+            )
+            .then(res => {
+              this.setState({
+                itemList: this.state.itemList.push(this.props.product.chosenItem)
+              });
+              console.log(this.state.itemList);
+            })
+            .catch(err => console.log(err)); // Tell them we can't load info?
+        }
+      })
+      .catch(err => {
+        console.log("");
+      });
   }
 
   public render() {
     return (
       <div className="row">
+        {/* {this.props.buyer.currentBuyer.boughtItems} */}
         <ProfileNavComponent />
         <div className="col-10">
-          This is the bought items Page.{" "}
-          {this.props.buyer.currentBuyer.username}'s previously purchased items
-          will be shown here.
-          <br />
-          We can access them with this.props.buyer.currentBuyer.boughtItems,
-          which is an array. We will insert a table to make it presentable and
-          map the info.
-          <br />
+          {/* HERE make conditional on being logged in so it doesn't break. */}
+
           <div className="row">
-            <div className="col">{this.props.buyer.currentBuyer.username}</div>
-            <div className="col">
-              {/* {this.props.buyer.currentBuyer.boughtItems} */}
-            </div>
+            <h2>
+              Previously purchased items for{" "}
+              {this.props.buyer.currentBuyer.username}
+            </h2>
+          </div>
+          <div className="row">
+            {this.state.itemList.length > 0 &&
+              this.state.itemList.map((product: any) => (
+                <div
+                  className="card pop-card home-pop-card"
+                  key={product.timePosted}
+                >
+                  {/* <div className="card-title">
+                  <h5>{product.name}</h5>
+                </div> */}
+                  <img
+                    className="card-img-top pop-card-img"
+                    src={
+                      "http://popbay-photo-storage.s3.amazonaws.com/" +
+                      product.photoNames[0]
+                    }
+                    alt="Card image cap"
+                  />
+                  <div className="card-title">
+                    <h5>{product.name}</h5>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      Category: {product.category}
+                    </li>
+                    <li className="list-group-item">Type: {product.type}</li>
+                    <li className="list-group-item">
+                      Condition: {product.condition}
+                    </li>
+                  </ul>
+                  <div className="card-body">
+                    a couple items
+                    {/* insert React-router-dom links instead
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a> */}
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
       </div>
--- src/components/bought/bought.container.tsx
+++ src/components/bought/bought.container.tsx
@@ -1,15 +1,18 @@
 import { connect } from "react-redux";
 import { IState } from "../../reducers";
 import { getBuyer } from "../../actions/buyer/buyer.actions";
+import { getBySellerAndTime } from "../../actions/product/product.actions";
 import { BoughtComponent } from "./bought.component";
 
 const mapStateToProps = (state: IState) => ({
   buyer: state.buyer,
+  product: state.product,
   user: state.user
 });
 
 export const mapDispatchToProps = {
-  getBuyer
+  getBuyer,
+  getBySellerAndTime
 };
 
 export const BoughtContainer = connect(
